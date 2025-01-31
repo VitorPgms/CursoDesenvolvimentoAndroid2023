17d7ac851435ba14d9be407ef08fd2f9354edf35
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2025-01-30 21:04:31 -0300

    Refatorando e organizando codigo

--- AppListaCurso/app/src/main/java/devandroid/depaula/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/depaula/applistacurso/controller/PessoaController.java
@@ -1,12 +1,23 @@
 package devandroid.depaula.applistacurso.controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import devandroid.depaula.applistacurso.model.Pessoa;
+import devandroid.depaula.applistacurso.view.MainActivity;
 
 public class PessoaController {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+    public static final String NOME_PREFERENCES = "pref_listavip";
+
+    public PessoaController(MainActivity mainActivity){
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        listaVip = preferences.edit();
+    }
 
     @NonNull
     @Override
@@ -17,7 +28,33 @@ public class PessoaController {
         return super.toString();
     }
 
+    public void criarSharedPreferences(){
+
+    }
+
     public void salvar(Pessoa pessoa) {
         Log.d("MVC_Controller","Salvo"+pessoa.toString());
+
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("cursoDesejado", pessoa.getCursoDesejado());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.apply();
+    }
+
+    public Pessoa buscar(Pessoa pessoa){
+
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
+        pessoa.setSobreNome(preferences.getString("sobreNome", ""));
+        pessoa.setCursoDesejado(preferences.getString("cursoDesejado", ""));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+
+        return pessoa;
+    }
+
+    public void limpar(){
+        listaVip.clear();
+        listaVip.apply();
+
     }
 }
